a95873f14c591ba76f5475ace61fbb13be373177
Rubel <mr.adcadre41@gmail.com>
2026-02-25 10:32:22 +0600

    selling purposes added

---
+++ functionAndArguments.js
@@ -0,0 +1,9 @@
+function kenaKata(num1, num2) {
+  const result = num1 + num2;
+  console.log(result);
+}
+kenaKata(5000, 6000);
+
+const shikha = 2500;
+const rubel = 5000;
+kenaKata(shikha, rubel);
